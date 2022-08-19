08205c3a106742774a4c8630c9903d72faa5c726
Rafael <rafaelcdmm@gmail.com>
2022-08-19 11:11:17 -0300

    completed lessons 1 and 2

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,11 @@
+describe('Home Page', () => {
+  it('the h1 contains the correct text', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-test="hero-heading"]').contains('Testing Next.js Applications with Cypress')
+  })
+
+  it.only('the features on the homepage are correct', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('dt').eq(0).contains('4 Courses')
+  })
+})
